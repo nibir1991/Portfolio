14df7814121397fa3219a59118f9323e8ff03dfc
Nibir <nibir.rayhan1991@gmail.com>
2020-11-18 02:15:16 +0600

    Portfolio in Progress

---
+++ static/js/custom.js
@@ -0,0 +1,17 @@
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
+
+
+
+
+
+
+
+
+  
